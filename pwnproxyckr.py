f6fdf8b203a186f8fa4378ce63e9664c5c4156b6
lowk <davidkevinit@gmail.com>
2025-01-14 13:16:23 +0700

    add option filter and enable color

--- pwnproxyckr.py
+++ pwnproxyckr.py
@@ -12,6 +12,9 @@ DOMAIN_PATTERN = re.compile(
     r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]"
 )
 
+FILTER_STATUS=2
+COLOR=True
+
 def fetch_cidrs(api_url, parser_func):
     response = requests.get(api_url)
     return parser_func(response)
@@ -63,11 +66,17 @@ def check_ip_cdn_status(ip):
 
 @click.command()
 @click.option("-t", "--threads", default=20, help="Number of threads")
-def process_input(threads):
+@click.option("-fs", "--status", default=2, help="Filter the output by status. ( 0 = Not Proxied, 1 = Proxied, 2 =  All )")
+@click.option("-c", "--color", default=True, help="Enable or disable color output")
+def process_input(threads, status, color):
     """
     Process input from standard input, determine if each line is an IP or domain,
     and check if it is served through Cloudflare or CloudFront.
     """
+    global FILTER_STATUS, COLOR
+    FILTER_STATUS = status
+    COLOR = color
+    
     with ThreadPoolExecutor(max_workers=threads) as executor:
         for line in sys.stdin:
             clean_line = line.strip().replace("https://", "").replace("http://", "")
@@ -75,26 +84,46 @@ def process_input(threads):
                 check_ip_cdn_status(clean_line)
             elif DOMAIN_PATTERN.fullmatch(clean_line):
                 executor.submit(check_domain_cdn_status, clean_line)
-            else:
+            elif FILTER_STATUS == 2:
                 print(f"Only IP and Domain Name are supported [{clean_line}]")
+
 
 def report_cdn_status(is_proxied, ip, domain=None):
     """
     Print the CDN status of the given IP address and/or domain name.
     """
-    reset = "\033[0m"
-    orange = "\033[33m"
-    green = "\033[32m"
-    blue = "\033[34m"
-    purple = "\033[35m"
+    global FILTER_STATUS, COLOR
 
-    status = f"{orange}🟠 Proxied{reset}" if is_proxied else f"{green}🟢 Not Proxied{reset}"
+    if not COLOR:
+        reset = ""
+        orange = ""
+        green = ""
+        blue = ""
+        purple = ""
+        mark = ""
+    else: 
+        reset = "\033[0m"
+        orange = "\033[33m"
+        green = "\033[32m"
+        blue = "\033[34m"
+        purple = "\033[35m"
+        mark = "🟠 " if is_proxied else "🟢 "
+    
+    status = ""
+    if FILTER_STATUS == 2:
+        status = f"{orange}{mark}Proxied{reset}" if is_proxied else f"{green}{mark}Not Proxied{reset} "
+
+
+    # Filter the output based on the status
+    if (FILTER_STATUS == 0 and is_proxied) or (FILTER_STATUS == 1 and not is_proxied):
+        return 
+
     ip_colored = f"{blue}{ip}{reset}"
     if domain:
         domain_colored = f"{purple}{domain}{reset}"
-        print(f"{status} [{ip_colored}] [{domain_colored}]")
+        print(f"{status}{ip_colored} {domain_colored}")
     else:
-        print(f"{status} [{ip_colored}]")
+        print(f"{status}{ip_colored}")
 
 if __name__ == "__main__":
     process_input()
